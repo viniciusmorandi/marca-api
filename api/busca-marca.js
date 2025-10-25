923ef8929f0250e0687d8818aeda3cf8b74f8e7a
viniciusmorandi <vinicius.morandi@gmail.com>
2025-10-25 12:14:53 -0300

    Adiciona suporte CORS para requisicoes cross-origin

--- api/busca-marca.js
+++ api/busca-marca.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+    // Adiciona headers CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Responde ao preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).send("Método inválido");
   }
